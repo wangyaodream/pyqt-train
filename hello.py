53763211b4c25a07b6b3f74da473bfd30168b405
wangyao <wangyao@mbpAnt.local>
2023-03-02 17:17:13 +0800

    init project

---
+++ hello.py
@@ -0,0 +1,16 @@
+import sys
+
+
+from PyQt6.QtWidgets import QApplication, QLabel, QWidget
+
+
+app = QApplication([])
+window = QWidget()
+window.setWindowTitle("First PyQt App")
+window.setGeometry(100, 100, 280, 80)
+msg = QLabel("<h1>Hello World!</h1>", parent=window)
+msg.move(60, 15)
+
+window.show()
+sys.exit(app.exec())
+
